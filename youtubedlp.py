81064daaf4eb6751e8822c6ef152a032ea6504e3
SMUELDigital <114599417+SMUELDigital@users.noreply.github.com>
2025-01-15 11:58:13 +0100

    Update youtubedlp.py

--- youtubedlp.py
+++ youtubedlp.py
@@ -1,3 +1,15 @@
+"""
+Disclaimer:
+This script is provided for educational and personal use only.
+
+1. Downloading videos from YouTube may violate its Terms of Service unless you have explicit permission or are using YouTube Premium.
+2. The user is responsible for ensuring compliance with local laws and regulations.
+3. The author assumes no liability for misuse of this script or any consequences resulting from its use.
+
+Use this script at your own risk.
+"""
+
+
 import yt_dlp
 
 def download_video(url):
